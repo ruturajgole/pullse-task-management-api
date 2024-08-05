3635f2113cb5899b17de18e16749e53e20166770
ruturajpokemon <ruturajpokemon1@gmail.com>
2024-08-05 18:00:32 +0530

    Change Port For Listening

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,15 @@ import taskRoutes from './routes/task';
 const fastify = Fastify({
   logger: true
 });
-fastify.register(cors, {origin: "http://localhost:3001"});
+fastify.register(cors, {origin: "http://localhost:3000"});
 fastify.register(authRoutes);
 fastify.register(taskRoutes);
 
 const start = async () => {
   try {
     await connectToDatabase();
-    await fastify.listen({ port: 3000 });
-    console.log('Server is running at http://localhost:3000');
+    await fastify.listen({ port: 3001 });
+    console.log('Server is running at http://localhost:3001');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
